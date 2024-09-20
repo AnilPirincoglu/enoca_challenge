153528ff60bd48d25beb88ba45e1cf053bfe8496
anilp. <anl.pirincoglu@gmail.com>
2024-09-20 21:59:50 +0300

    Add ProductRepository interface for Product entity - Define ProductRepository interface extending JpaRepository - Enable CRUD operations for Product entity

---
+++ src/main/java/dev/anilp/enoca_challenge/product/ProductRepository.java
@@ -0,0 +1,8 @@
+package dev.anilp.enoca_challenge.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
