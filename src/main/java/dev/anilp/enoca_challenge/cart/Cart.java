b59e4f5b6fc8e2f8e5c9c8d7d5fe7b0a4304eb1d
anilp. <anl.pirincoglu@gmail.com>
2024-09-21 00:02:58 +0300

    Feat Cart: Add OneToMany relationship between Cart and CartItem
    - Added OneToMany annotation to Cart class for cartItems field
    - Ensured cascade operations and orphan removal for cartItems

--- src/main/java/dev/anilp/enoca_challenge/cart/Cart.java
+++ src/main/java/dev/anilp/enoca_challenge/cart/Cart.java
@@ -1,10 +1,13 @@
 package dev.anilp.enoca_challenge.cart;
 
 import dev.anilp.enoca_challenge.BaseEntity;
+import dev.anilp.enoca_challenge.cartItem.CartItem;
 import dev.anilp.enoca_challenge.customer.Customer;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity(name = "Cart")
@@ -22,6 +25,9 @@ public class Cart extends BaseEntity {
     @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
     @JoinColumn(name = "customer_id", referencedColumnName = "id", nullable = false, foreignKey = @ForeignKey(name = "cart_customer_id_fk"))
     private Customer customer;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
 
     public Cart() {
         this.totalAmount = BigDecimal.ZERO;
@@ -58,17 +64,21 @@ public class Cart extends BaseEntity {
         this.customer = customer;
     }
 
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Cart cart)) return false;
         if (!super.equals(o)) return false;
-        return Objects.equals(id, cart.id) && Objects.equals(totalAmount, cart.totalAmount) && Objects.equals(customer, cart.customer);
+        return Objects.equals(id, cart.id) && Objects.equals(totalAmount, cart.totalAmount) && Objects.equals(customer, cart.customer) && Objects.equals(cartItems, cart.cartItems);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), id, totalAmount, customer);
+        return Objects.hash(super.hashCode(), id, totalAmount, customer, cartItems);
     }
 
     @Override
@@ -77,6 +87,7 @@ public class Cart extends BaseEntity {
                 "id=" + id +
                 ", totalAmount=" + totalAmount +
                 ", customer=" + customer +
+                ", cartItems=" + cartItems +
                 ", createdAt=" + getCreatedAt() +
                 ", updatedAt=" + getUpdatedAt() +
                 '}';
