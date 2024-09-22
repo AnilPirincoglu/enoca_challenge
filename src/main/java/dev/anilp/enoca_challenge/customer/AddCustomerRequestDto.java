32152dccf86626f71ad16ff5556cd90a67c0ddcb
anilp. <anl.pirincoglu@gmail.com>
2024-09-23 00:22:41 +0300

    Add AddCustomerRequestDto record for customer creation requests
    - Introduced `AddCustomerRequestDto` record to encapsulate customer creation request data.
    - Added validation annotations to ensure proper data constraints:
      - `name` and `lastName` fields must be between 2 and 100 characters.
      - `email` field must be a valid email address and less than 150 characters.

---
+++ src/main/java/dev/anilp/enoca_challenge/customer/AddCustomerRequestDto.java
@@ -0,0 +1,15 @@
+package dev.anilp.enoca_challenge.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public record AddCustomerRequestDto(
+        @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+        String name,
+        @Size(min = 2, max = 100, message = "Last name must be between 2 and 100 characters")
+        String lastName,
+        @Email(message = "Email should be valid")
+        @Size(max = 150, message = "Email must be less than 150 characters")
+        String email
+) {
+}
