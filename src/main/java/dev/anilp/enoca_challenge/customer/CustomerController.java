42a8d3a3acab19dcb549547a6f907497ab9d0323
anilp. <anl.pirincoglu@gmail.com>
2024-09-23 00:51:49 +0300

    Add CustomerController to handle customer-related HTTP requests
    - Added `CustomerController` to manage customer-related endpoints.
    - Implemented `addCustomer` method to handle POST requests for adding new customers.
    - Integrated `CustomerService` to process customer addition logic.

---
+++ src/main/java/dev/anilp/enoca_challenge/customer/CustomerController.java
@@ -0,0 +1,25 @@
+package dev.anilp.enoca_challenge.customer;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public void addCustomer(@Valid @RequestBody AddCustomerRequestDto request) {
+        customerService.addCustomer(request);
+    }
+}
