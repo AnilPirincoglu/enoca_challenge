310861d530268fa9c0b93e58f4e0cf350d9d40b4
anilp. <anl.pirincoglu@gmail.com>
2024-09-22 20:15:01 +0300

    Feat CustomerRepository: Add custom query to check if customer email exists
    - Added a custom query method `existsByEmail` in `CustomerRepository` to check if a customer email exists in the database.

--- src/main/java/dev/anilp/enoca_challenge/customer/CustomerRepository.java
+++ src/main/java/dev/anilp/enoca_challenge/customer/CustomerRepository.java
@@ -1,8 +1,11 @@
 package dev.anilp.enoca_challenge.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Customer c WHERE c.email = :email")
+    boolean existsByEmail(String email);
 }
