fe545eff5059948629aa63f87bcea0711aea45eb
anilp. <anl.pirincoglu@gmail.com>
2024-09-23 16:55:20 +0300

    Feat ProductRepository: Add custom query to check if a product exists by name
    - Implemented a custom query using the @Query annotation in ProductRepository.
    - The query checks if a product with a given name exists in the database.

--- src/main/java/dev/anilp/enoca_challenge/product/ProductRepository.java
+++ src/main/java/dev/anilp/enoca_challenge/product/ProductRepository.java
@@ -1,8 +1,11 @@
 package dev.anilp.enoca_challenge.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Product p WHERE p.name = :name")
+    Boolean existsProductByName(String name);
 }
