cda5ac06d8ef6755f4b76b6ef060d322837c8376
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-09-25 12:51:32 +0300

    Add findByName query to ProductRepository
    - Implement findByName method in ProductRepository to retrieve a product by its name
    - Use @Query annotation to define the custom query
    - Return an Optional<Product> to handle cases where the product may not be found

--- src/main/java/dev/anilp/enoca_challenge/product/ProductRepository.java
+++ src/main/java/dev/anilp/enoca_challenge/product/ProductRepository.java
@@ -3,9 +3,13 @@ package dev.anilp.enoca_challenge.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Product p WHERE p.name = :name")
     Boolean existsProductByName(String name);
+
+    @Query("SELECT p FROM Product p WHERE p.name = :name")
+    Optional<Product> findByName(String name);
 }
