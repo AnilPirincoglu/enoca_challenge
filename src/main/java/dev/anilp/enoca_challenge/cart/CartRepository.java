b11bf87bbfefe5b8038ec370926d7fb6197a57b4
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-09-28 18:57:20 +0300

    Feat CartRepository: Add findCartByCustomerId query to CartRepository
    - Implemented `findCartByCustomerId` method in `CartRepository.java`
    - Added query to fetch cart details by customer ID
    - Updated unit tests to cover the new method

--- src/main/java/dev/anilp/enoca_challenge/cart/CartRepository.java
+++ src/main/java/dev/anilp/enoca_challenge/cart/CartRepository.java
@@ -1,6 +1,12 @@
 package dev.anilp.enoca_challenge.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Query("SELECT c FROM Cart c WHERE c.customer.id = :customerId")
+    Optional<Cart> findCartByCustomerId(UUID customerId);
 }
