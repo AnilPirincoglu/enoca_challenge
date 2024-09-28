ff8831a3a8609055980c96832baaa74ad88e0a48
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-09-28 22:28:22 +0300

    Refactor CartService class
    - Added `updateItemQuantity` and `addNewItemToCart` methods to improve code readability.
    - Updated `addProductToCart` method to use the new helper methods.
    - Added `createNewCartItem` method to streamline cart item creation in `updateCart`.
    - Improved logging for better traceability.

--- src/main/java/dev/anilp/enoca_challenge/cart/CartService.java
+++ src/main/java/dev/anilp/enoca_challenge/cart/CartService.java
@@ -6,6 +6,7 @@ import dev.anilp.enoca_challenge.cart.util.dto.RemoveProductFromCartRequestDto;
 import dev.anilp.enoca_challenge.cart.util.dto.UpdateCartRequestDto;
 import dev.anilp.enoca_challenge.cart_item.CartItem;
 import dev.anilp.enoca_challenge.cart_item.CartItemId;
+import dev.anilp.enoca_challenge.cart_item.util.dto.CartItemRequestDto;
 import dev.anilp.enoca_challenge.customer.CustomerService;
 import dev.anilp.enoca_challenge.exception.ErrorMessage;
 import dev.anilp.enoca_challenge.exception.exceptions.ResourceNotFoundException;
@@ -42,25 +43,6 @@ public class CartService {
     }
 
     @Transactional
-    public void updateCart(Long cartId, UpdateCartRequestDto requestDto) {
-        Cart cart = findCartById(cartId);
-
-        List<CartItem> cartItems = requestDto.cartItems().stream()
-                .map(request -> {
-                    Product product = productService.findProductByName(request.productName());
-                    productService.checkStockQuantity(product, request.quantity());
-                    return newCartItem(cart, product, request.quantity());
-                })
-                .toList();
-
-        log.info("Updating cart with id: {}", cartId);
-        cart.getCartItems().clear();
-        cart.getCartItems().addAll(cartItems);
-        cart.calculateTotalAmount();
-        cartRepository.save(cart);
-    }
-
-    @Transactional
     public void addProductToCart(AddProductToCartRequestDto requestDto, @Positive(message = "Quantity must be a positive integer.") Integer quantity) {
         Cart cart = getCustomerCart(requestDto.customerId());
 
@@ -70,18 +52,44 @@ public class CartService {
         cart.getCartItems().stream()
                 .filter(item -> item.getProduct().getName().equals(requestDto.productName()))
                 .findFirst()
-                .ifPresentOrElse(cartItem -> {
-                            log.info("Incrementing quantity of product {} in cart with id: {}", cartItem.getProduct().getName(), cart.getId());
-                            cartItem.updateQuantity(quantity);
-                        },
-                        () -> {
-                            log.info("Adding product {} to cart with id: {}", product.getName(), cart.getId());
-                            cart.getCartItems().add(newCartItem(cart, product, quantity));
-                            log.info("{}", cart.getCartItems());
-                        });
+                .ifPresentOrElse(cartItem -> updateItemQuantity(cart, cartItem, quantity),
+                        () -> addNewItemToCart(cart, product, quantity));
 
         cart.calculateTotalAmount();
         cartRepository.save(cart);
+    }
+
+    private void updateItemQuantity(Cart cart, CartItem cartItem, Integer quantity) {
+        log.info("Updating quantity of cart item {} in cart with id: {}", cartItem.getProduct().getName(), cart.getId());
+        cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        cartItem.calculateTotalAmount();
+        log.info("Cart item updated: {}, total amount: {}", cartItem.getProduct().getName(), cartItem.getTotalAmount());
+    }
+
+    private void addNewItemToCart(Cart cart, Product product, Integer quantity) {
+        log.info("Adding product {} to cart with id: {}", product.getName(), cart.getId());
+        cart.getCartItems().add(newCartItem(cart, product, quantity));
+    }
+
+    @Transactional
+    public void updateCart(Long cartId, UpdateCartRequestDto requestDto) {
+        Cart cart = findCartById(cartId);
+
+        List<CartItem> cartItems = requestDto.cartItems().stream()
+                .map(request -> createNewCartItem(request, cart))
+                .toList();
+
+        log.info("Updating cart with id: {}", cartId);
+        cart.getCartItems().clear();
+        cart.getCartItems().addAll(cartItems);
+        cart.calculateTotalAmount();
+        cartRepository.save(cart);
+    }
+
+    private CartItem createNewCartItem(CartItemRequestDto request, Cart cart) {
+        Product product = productService.findProductByName(request.productName());
+        productService.checkStockQuantity(product, request.quantity());
+        return newCartItem(cart, product, request.quantity());
     }
 
     @Transactional
@@ -91,29 +99,32 @@ public class CartService {
         cart.getCartItems().stream()
                 .filter(item -> item.getProduct().getName().equals(productName))
                 .findFirst()
-                .ifPresentOrElse(cartItem -> {
-                    cart.getCartItems().remove(cartItem);
-                    cart.calculateTotalAmount();
-                    log.info("Removing product {} from cart with id: {}", productName, cart.getId());
-                    cartRepository.save(cart);
-                }, () -> {
-                    log.info("Product {} not found in cart with id: {}", productName, cart.getId());
-                    throw new ResourceNotFoundException(ErrorMessage.PRODUCT_NOT_IN_CART, productName);
-                });
+                .ifPresentOrElse(cartItem -> removeItemFormCart(cart, cartItem),
+                        () -> {
+                            throw new ResourceNotFoundException(ErrorMessage.PRODUCT_NOT_IN_CART, productName);
+                        });
+    }
+
+    private void removeItemFormCart(Cart cart, CartItem cartItem) {
+        log.info("Removing product {} from cart with id: {}", cartItem.getProduct().getName(), cart.getId());
+        cart.getCartItems().remove(cartItem);
+        cart.calculateTotalAmount();
+        cartRepository.save(cart);
     }
 
     @Transactional
     public void emptyCart(Long cartId) {
         Cart cart = findCartById(cartId);
+        log.info("Emptying cart with id: {}", cartId);
         cart.getCartItems().clear();
         cart.calculateTotalAmount();
-        log.info("Emptying cart with id: {}", cartId);
         cartRepository.save(cart);
     }
 
     private CartItem newCartItem(Cart cart, Product product, int quantity) {
         CartItem cartItem = new CartItem(new CartItemId(cart.getId(), product.getId()), quantity, cart, product);
         cartItem.calculateTotalAmount();
+        log.info("Cart item created for product: {}, total amount: {}", product.getName(), cartItem.getTotalAmount());
         return cartItem;
     }
 
