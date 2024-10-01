54bdf4bde98e384540932d031282c8fdd566ee3f
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-02 01:40:44 +0300

    Add ORDER_NOT_FOUND_WITH_GIVEN_CODE error message to ErrorMessage enum
    - Added `ORDER_NOT_FOUND_WITH_GIVEN_CODE` to `ErrorMessage` enum to handle order not found errors.

--- src/main/java/dev/anilp/enoca_challenge/exception/ErrorMessage.java
+++ src/main/java/dev/anilp/enoca_challenge/exception/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     PRODUCT_NOT_FOUND_WITH_GIVEN_NAME("Product not found with given name: "),
     PRODUCT_NOT_IN_CART("Product not in your cart with given name: "),
     INSUFFICIENT_STOCK("Insufficient stock! Available stock: "),
-    CART_NOT_FOUND_WITH_GIVEN_ID("Cart not found with given id: ");
+    CART_NOT_FOUND_WITH_GIVEN_ID("Cart not found with given id: "),
+    ORDER_NOT_FOUND_WITH_GIVEN_CODE("Order not found with given code: ");
 
     private final String message;
 
